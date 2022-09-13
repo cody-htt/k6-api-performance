ed62858a23633126fc67c00344f6c6f2b1f5fed4
tung.huynh <tung@xload.io>
2022-09-14 02:37:11 +0700

    test: update test files

--- src/tests/get-200-status.test.ts
+++ src/tests/get-200-status.test.ts
@@ -1,17 +1,18 @@
-import { sleep, check } from 'k6';
+import { sleep } from 'k6';
 import { Options } from 'k6/options';
+import { verifyResStatus } from '../utils/verifyResponse';
+import { checkErrorRate } from '../utils/checkErrorRate';
 import http from 'k6/http';
 
 export let options: Options = {
   vus: 5,
-  duration: '5s',
+  duration: '10s',
   iterations: 20,
 };
 
 export default () => {
   const res = http.get('https://test-api.k6.io');
-  check(res, {
-    'status is 200': () => res.status === 200,
-  });
+  verifyResStatus('Check res status 200', res, 200);
+  checkErrorRate(res);
   sleep(1);
 };
--- src/tests/post-400-status.test.ts
+++ src/tests/post-400-status.test.ts
@@ -1,20 +1,19 @@
-import { sleep, check } from 'k6';
+import { sleep } from 'k6';
 import { Options } from 'k6/options';
-
-/* @ts-ignore */
-import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
+import { verifyResStatus } from '../utils/verifyResponse';
+import { checkErrorRate } from '../utils/checkErrorRate';
+import { randomNumber } from '../utils/randomNumber';
 import http from 'k6/http';
 
 export let options: Options = {
   vus: 5,
-  duration: '5s',
+  duration: '10s',
   iterations: 20,
 };
 
 export default () => {
   const res = http.post('https://httpbin.org/status/400');
-  check(res, {
-    'status is 400': () => res.status === 400,
-  });
-  sleep(randomIntBetween(1, 5));
+  verifyResStatus('Check Response Status 400', res, 400);
+  checkErrorRate(res);
+  sleep(randomNumber(1, 5));
 };
